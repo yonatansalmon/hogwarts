41a0645c2831a5ece89d182bf6ba6ddafc5af415
Yonatan Salmon <yonatansalmon@gmail.com>
2020-01-14 10:16:27 +0200

    changed forms

--- hogwarts.py
+++ hogwarts.py
@@ -1,9 +1,10 @@
 import threading
 import time
-import json
+from flask import Flask, jsonify, render_template, request, redirect, url_for
+from student_list import students, courses
 
-from flask import Flask, jsonify, render_template, request
-from student_list import students
+
+count = 5
 
 app = Flask(__name__)
 
@@ -11,27 +12,27 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def get_students():
     students_list = students
-
     return render_template('show_student.html', students_list=students_list)
 
 
 @app.route('/add_student', methods=['POST', 'GET'])
 def add_students():
     students_list = students
+    courses_list = courses
     if request.method == 'POST':
         fname = request.form.get('First_Name')
         lname = request.form.get('Last_Name')
-        course = request.form.get('Creation_Time')
-        students_list.append({"first_name": fname, "last_name": lname, "creation_time": course})
-        return "student added"
+        course = request.form.get('Course_Interest')
+        magic_skillz = request.form.getlist('Magic_skillz[]')
+        desired_skillz = request.form.getlist('Desired_skillz[]')
+        last_id = str(int(students_list[-1]["id"]) + 1)
+        students_list.append({"id": last_id, "first_name": fname, "last_name": lname, \
+                              "creation_time": time.ctime(), "last_update": time.ctime(), \
+                              "existing_skills": magic_skillz, "desired_skills": desired_skillz,
+                              "course_interest": [course], })
+        return redirect('/')
     else:
-        return render_template('add_student.html')
-
-
-@app.route('/edit_student', methods=["GET"])
-def edit_student():
-    students_list = students
-    return render_template("edit_student.html", students_list=students_list)
+        return render_template('add_student.html', courses_list=courses_list)
 
 
 @app.route("/students/<int:id>", methods=['GET'])
@@ -48,17 +49,31 @@ def get_student2(id):
     if request.method == 'GET':
         return render_template('edit_student.html', student=student)
     elif request.method == 'POST':
-        a = request.form.get('last_name')
-        student['last_name'] = a
+        fname = request.form.get('first_name')
+        lname = request.form.get('last_name')
+        cinterest = request.form.get('course_interest')
+        cinterest_split = cinterest.split(",")
+        dskills = request.form.get('desired_skills')
+        dskills_split = dskills.split(",")
+        eskills = request.form.get('existing_skills')
+        eskills_split = eskills.split(",")
+        student['first_name'] = fname
+        student['last_name'] = lname
+        student['course_interest'] = cinterest_split
+        student['desired_skills'] = dskills_split
+        student['desired_skills'] = eskills_split
         return render_template('student.html', student=student)
 
 
+@app.after_request
+def add_header(r):
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
 
 if __name__ == "__main__":
     time.sleep(0.5)
     threading.Thread(target=app.run).start()
-
-
-
-
--- student_list.py
+++ student_list.py
@@ -2,14 +2,9 @@ import time
 import random
 
 magic_skills = ["Alchemy", "Animation", "Conjuror", "Disintegration", "Elemental", "Healing", "Illusion", "Immortality", "Invisibility", "Invulnerability", "Necromancer", "Omnipresent", "Omniscient", "Poison", "Possession", "Self-detonation", "Summoning", "Water breathing"]
-enum_skills = [(i,j) for i,j in enumerate(magic_skills)]
-existing_skills = random.choices(enum_skills, k=5)
-existing_skillsToStr = ' '.join([str(elem) for elem in existing_skills])
-desired_skills = random.choices(enum_skills, k=2)
-desired_skillsToStr = ' '.join([str(elem) for elem in desired_skills])
 
 courses = ["Alchemy basics", "Alchemy advanced", "Magic for day-to-day life", "Magic for medical professionals", "Dating with magic",]
-course_interest = random.choices(courses, k=2)
+
 
 
 students = [
@@ -19,18 +14,18 @@ students = [
         "last_name": "Salmon",
         "creation_time": time.ctime(),
         "last_update": time.ctime(),
-        "existing_skills": existing_skillsToStr,
-        "desired_skills": desired_skillsToStr,
-        "course_interest": course_interest,
+        "existing_skills": random.choices(magic_skills, k=5),
+        "desired_skills": random.choices(magic_skills, k=2),
+        "course_interest": random.choices(courses, k=2),
     },
         {"id": 2,
         "first_name": "Mark",
         "last_name": "Something",
         "creation_time": time.ctime(),
         "last_update": time.ctime(),
-        "existing_skills": existing_skillsToStr,
-        "desired_skills": desired_skillsToStr,
-        "course_interest":course_interest,
+        "existing_skills": random.choices(magic_skills, k=5),
+        "desired_skills": random.choices(magic_skills, k=2),
+        "course_interest": random.choices(courses, k=2),
     },
     {
         "id": 3,
@@ -38,18 +33,18 @@ students = [
         "last_name": "Mosokfn",
         "creation_time": time.ctime(),
         "last_update": time.ctime(),
-        "existing_skills": existing_skillsToStr,
-        "desired_skills": desired_skillsToStr,
-        "course_interest":course_interest,
+        "existing_skills": random.choices(magic_skills, k=5),
+        "desired_skills": random.choices(magic_skills, k=2),
+        "course_interest":random.choices(courses, k=2),
     },
     {"id": 4,
         "first_name": "Susan",
         "last_name": "Salmafafafafon",
         "creation_time": time.ctime(),
         "last_update": time.ctime(),
-        "existing_skills": existing_skillsToStr,
-        "desired_skills": desired_skillsToStr,
-        "course_interest":course_interest,
+        "existing_skills": random.choices(magic_skills, k=5),
+        "desired_skills": random.choices(magic_skills, k=2),
+        "course_interest":random.choices(courses, k=2),
      },
 
     {"id": 5,
@@ -57,8 +52,8 @@ students = [
      "last_name": "yfyuSalmafafafafon",
      "creation_time": time.ctime(),
      "last_update": time.ctime(),
-     "existing_skills": existing_skillsToStr,
-     "desired_skills": desired_skillsToStr,
-     "course_interest": course_interest,
+     "existing_skills":random.choices(magic_skills, k=5),
+     "desired_skills": random.choices(magic_skills, k=2),
+     "course_interest": random.choices(courses, k=2),
      },
 ]
