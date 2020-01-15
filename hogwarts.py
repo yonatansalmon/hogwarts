f5bd88145e3e621d59e08373dc7a11dc80d1f8b2
Yonatan Salmon <yonatansalmon@gmail.com>
2020-01-15 16:51:57 +0200

    added pie charts

--- hogwarts.py
+++ hogwarts.py
@@ -1,7 +1,7 @@
 import threading
 import time
 from flask import Flask, jsonify, render_template, request, redirect, url_for
-from student_list import students, courses
+from student_list import students, courses, labels, colors, magic_skills
 
 count = 5
 
@@ -36,7 +36,8 @@ def add_students():
                               "last_update": time.ctime(),
                               "existing_skills": magic_skills,
                               "desired_skills": desired_skills,
-                              "course_interest": [course], })
+                              "course_interest": [course],
+                              })
         return redirect('/')
     else:
         return render_template('add_student.html', courses_list=courses_list)
@@ -74,6 +75,37 @@ def get_student2(id):
         return redirect('/')
     else:
         return render_template("student_edit.html", student=student)
+
+
+@app.route("/student_stats", methods=["GET"])
+def student_stats():
+    student_list = students
+    existing_skills = []
+    desired_skills = []
+    student = [student for student in student_list]
+    for skill in student:
+        eskills = skill["existing_skills"]
+        dskills = skill["desired_skills"]
+        for skill in eskills:
+            student_skills = skill["skill"]
+            existing_skills.append(student_skills)
+        for skill in dskills:
+            desired_skills.append(skill)
+
+    existing_list = []
+    for skill in magic_skills:
+        count = existing_skills.count(skill)
+        if count:
+            existing_list.append({'y': count, 'label': skill})
+
+    desired_list = []
+    for skill in magic_skills:
+        count = desired_skills.count(skill)
+        if count:
+            desired_list.append({'y': count, 'label': skill})
+
+
+    return render_template("student_stats.html",student_list=student_list,counts_existing=existing_list,counts_desired=desired_list)
 
 
 @app.after_request
--- student_list.py
+++ student_list.py
@@ -3,7 +3,7 @@ import random
 
 magic_skills = ["Alchemy", "Animation", "Conjuror", "Disintegration", "Elemental", "Healing", "Illusion", "Immortality",
                 "Invisibility", "Invulnerability", "Necromancer", "Omnipresent", "Omniscient", "Poison", "Possession",
-                "Self-detonation", "Summoning", "Water breathing"]
+                "Self-detonation", "Summoning", "Water-breathing"]
 level = [1, 2, 3, 4, 5]
 
 courses = ["Alchemy basics", "Alchemy advanced", "Magic for day-to-day life", "Magic for medical professionals",
@@ -113,4 +113,21 @@ students = [
      "desired_skills": random.choices(magic_skills, k=2),
      "course_interest": random.choices(courses, k=2),
      },
+]
+
+
+
+labels = [
+    'Alchemy', 'Animation', 'Conjuror', 'Disintegration',
+    'Elemental', 'Healing', 'Illusion', 'Immortality',
+    'Invisibility', 'Invulnerability', 'Necromancer', 'Omnipresent',
+    "Omniscient", "Poison", "Possession", "Self-detonation", "Summoning",
+    "Water breathing"]
+
+colors = [
+    "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA",
+    "#ABCDEF", "#DDDDDD", "#ABCABC", "#4169E1",
+    "#C71585", "#FF4500", "#FEDCBA", "#8B0000",
+    "#7FFF00", "#6495ED", "#00008B", "	#A9A9A9",
+    "#8B008B", "#FF1493"
 ]
